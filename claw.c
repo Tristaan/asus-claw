f4945d4bf6e85545460cc2424394f517d258ac16
Tristaan <tristan.sneider@gmail.com>
2016-10-19 01:57:53 +0200

    Prepared functions for sending data. Also added files with the control packets from the windows driver.

--- claw.c
+++ claw.c
@@ -4,28 +4,80 @@
 
 uint16_t VENDOR=0x195d;
 uint16_t PRODUCT=0x1016;
-libusb_device_handle* findClaw(struct libusb_device  **list){
-  int i=0;
-  struct libusb_device *dev;
-  struct libusb_device_handle *handle=NULL;
 
-  while((dev=list[i++])!=NULL){
-    struct libusb_device_descriptor desc;
-    int r = libusb_get_device_descriptor(dev, &desc);
-    if (r < 0) {
-      fprintf(stderr, "failed to get device descriptor");
-      return NULL;
+int sendClaw(struct libusb_device_handle *handle, unsigned char buffer[8]);
+int writeSettings(struct libusb_device_handle *handle,unsigned char buffer[12][8]);
+void transferComplete(struct libusb_transfer *transfer);
+
+int main (int argc, char *argv[]){
+  struct libusb_context *ctx;
+  struct libusb_device **list;
+
+  libusb_init(&ctx);
+  if(libusb_get_device_list(ctx,&list)<0){
+    fprintf(stderr,"Couldn't get device list.");
+    return 1;
+  }
+  struct libusb_device_handle *handle=libusb_open_device_with_vid_pid(ctx,VENDOR,PRODUCT);
+  if(handle==NULL){
+    libusb_free_device_list(list,1);
+    libusb_exit(ctx);
+    return -1;
+  }
+  //Handle DPI BUTTON INTERFACE -> Move to function?
+  libusb_detach_kernel_driver(handle,2); //Third interface are DPI buttons.EP 0x83
+  if(libusb_claim_interface(handle,2)){
+    fprintf(stderr,"Couldn't claim interface.");
+    return 1;
+  }
+
+  if(argc>1){
+    printf("Yep the argument is a file.");
+  }
+  struct libusb_transfer *transfer=libusb_alloc_transfer(0);
+  unsigned char *dataR;
+  int size=8;
+  dataR=malloc(size);
+  int completed=0;
+  libusb_fill_interrupt_transfer(transfer,handle,0x83,dataR,size,transferComplete,NULL,0);
+
+
+  while (!completed) {
+    if(libusb_submit_transfer(transfer)<0){
+      fprintf(stderr,"Error while submitting transfer.");
+      libusb_free_transfer(transfer);
     }
-
-    if(desc.idVendor==VENDOR && desc.idProduct==PRODUCT){
-      if(libusb_open(dev,&handle)){
-        fprintf(stderr,"Device couldn't be opened.");
-        return NULL;
-      }
-      return handle;
+    if (libusb_handle_events_completed(ctx, NULL) < 0){   // negative values are errors
+      fprintf(stderr,"handle events error");
+      break;
     }
   }
-  return NULL;
+  libusb_free_device_list(list,1);
+  libusb_exit(ctx);
+  printf("completed!");
+
+  return 0;
+}
+int sendClaw(struct libusb_device_handle *handle, unsigned char buffer[8]){
+  struct libusb_transfer *transfer=NULL;
+  libusb_fill_control_transfer(transfer,handle,buffer,transferComplete,NULL,5000);
+  if(libusb_submit_transfer(transfer)<0){
+    fprintf(stderr,"Couldn't submit transfer");
+    libusb_free_transfer(transfer);
+    return -2;
+  }
+  return 1;
+}
+
+int writeSettings(struct libusb_device_handle *handle,unsigned char buffer[12][8]){
+  for(int i=0;i<12;i++){
+    int r=0;
+    if((r=sendClaw(handle,buffer[i]))<0){
+      fprintf(stderr,"Error: %d",r);
+      return -1;
+    }
+  }
+  return 1;
 }
 
 void transferComplete(struct libusb_transfer *transfer){
@@ -58,52 +110,4 @@ void transferComplete(struct libusb_transfer *transfer){
       break;
   }
   fflush(stderr);
-}
-
-int main (){
-  struct libusb_context *ctx;
-  struct libusb_device **list;
-
-  libusb_init(&ctx);
-  if(libusb_get_device_list(ctx,&list)<0){
-    fprintf(stderr,"Couldn't get device list.");
-    return 1;
-  }
-  struct libusb_device_handle *handle=findClaw(list);
-  if(handle==NULL){
-    libusb_free_device_list(list,1);
-    libusb_exit(ctx);
-    return -1;
-  }
-  //Handle DPI BUTTON INTERFACE -> Move to function?
-  libusb_detach_kernel_driver(handle,2); //Third interface are DPI buttons.EP 0x83
-  if(libusb_claim_interface(handle,2)){
-    fprintf(stderr,"Couldn't claim interface.");
-    return 1;
-  }
-
-  struct libusb_transfer *transfer=libusb_alloc_transfer(0);
-  unsigned char *dataR;
-  int size=8;
-  dataR=malloc(size);
-  int completed=0;
-
-  libusb_fill_interrupt_transfer(transfer,handle,0x83,dataR,size,transferComplete,NULL,5000);
-
-
-  while (!completed) {
-    if(libusb_submit_transfer(transfer)<0){
-      fprintf(stderr,"Error while submitting transfer.");
-      libusb_free_transfer(transfer);
-    }
-    if (libusb_handle_events_completed(ctx, NULL) < 0){   // negative values are errors
-      fprintf(stderr,"handle events error");
-      break;
-    }
-  }
-  libusb_free_device_list(list,1);
-  libusb_exit(ctx);
-  printf("completed!");
-
-  return 0;
 }
